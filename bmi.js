0ddc6c9fb9a2eb1627c5ee0aeb8df013ccaf7cff
wml-9 <71692977+wml-9@users.noreply.github.com>
2020-10-09 10:06:51 +0800

    Add files via upload

---
+++ bmi.js
@@ -0,0 +1,18 @@
+let MarkMass = 98;
+let MarkHeight = 160;
+let JohnMass = 90;
+let JohnHeight = 162;
+console.log("Mark的BMI:")
+let BMI1 = MarkMass/(MarkHeight*MarkHeight)
+console.log(BMI1.toPrecision(1))
+console.log("John的BMI:")
+let BMI2 = JohnMass/(JohnHeight*JohnHeight)
+console.log(BMI2.toPrecision(1))
+
+console.log('Mark的BMI是否比John更高？')
+if(MarkHeight>JohnHeight){
+    console.log('ture')
+}
+else{
+    console.log('fales')
+}
